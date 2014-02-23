7e9dfe1dd33e3945a0df704e9ab7715b0d4629cf
Jung Lisa <yungij@andrew.cmu.edu>
2014-02-23 00:23:00 -0500

    added radom comment in article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
 	belongs_to :category
 
-	scope :active, where('active = ?', true)
+	scope :active, where('active = ?', true) #alsdkfja;lkfja;lkfej;alwkefja;lekwfja;kl
   	scope :alphabetical, order('title')
 end
